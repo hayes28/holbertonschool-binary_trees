6424090a75e3d331ec480dc534c8bfc395e606b3
hayes28 <heather.hayes918@gmail.com>
2022-12-19 19:07:50 -0800

    6-pre

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,7 +8,7 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!func && tree)
+	if (func && tree)
 	{
 		(func)(tree->n);
 		binary_tree_preorder(tree->left, func);
